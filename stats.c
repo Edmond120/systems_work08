9ace4403fb2e0081f647be5b98c8c2bb8327afe8
edmond120 <edmondwong888@msn.com>
2017-10-31 11:52:48 -0400

    started

---
+++ stats.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int main(){
+	struct stat buffer;
+	stat("test.txt",&buffer);
+	printf("size:%ld\xn",buffer.st_size);
+	printf("mode(permissions): %ld\n",buffer.st_mode);
+	printf("time of last access: %s\n",ctime(&buffer.st_atime));
+	return 0;
+}
