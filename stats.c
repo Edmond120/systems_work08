d9b28634b03a51f04500d35df5fc69bd850fad7d
edmond120 <edmondwong888@msn.com>
2017-11-01 19:19:57 -0400

    fixed very embarrassing

--- stats.c
+++ stats.c
@@ -4,9 +4,9 @@
 
 int main(){
 	struct stat buffer;
-	stat("test.txt",&buffer);
+	stat("text.txt",&buffer);
 	printf("size:%ld\n",buffer.st_size);
-	printf("mode(permissions): %ld\n",buffer.st_mode);
+	printf("mode(permissions): %3o\n",buffer.st_mode&0777);
 	printf("time of last access: %s\n",ctime(&buffer.st_atime));
 	return 0;
 }
