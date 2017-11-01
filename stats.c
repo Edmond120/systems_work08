21d99d51b65e1058b13e22d479684a43cf0095a1
edmond120 <edmondwong888@msn.com>
2017-11-01 19:38:18 -0400

    made size more readable

--- stats.c
+++ stats.c
@@ -5,7 +5,11 @@
 int main(){
 	struct stat buffer;
 	stat("text.txt",&buffer);
-	printf("size:%ld\n",buffer.st_size);
+	long bytes = buffer.st_size;
+	int gigabytes = bytes / 1000000000;
+	int megabytes = (bytes - (gigabytes * 1000000000)) / 1000000;
+	bytes = bytes - (megabytes*1000000 + gigabytes*1000000000);
+	printf("size: gb:%d mb:%d b:%ld\n",gigabytes,megabytes,bytes);
 	printf("mode(permissions): %3o\n",buffer.st_mode&0777);
 	printf("time of last access: %s\n",ctime(&buffer.st_atime));
 	return 0;
