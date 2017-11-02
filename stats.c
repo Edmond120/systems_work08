06383480b244e130526d1b70362199798deecb67
Jeffrey Lin <jeffrey@icurse.nl>
2017-11-01 20:49:22 -0400

    stats.c: better size calculation

--- stats.c
+++ stats.c
@@ -2,15 +2,21 @@
 #include <sys/stat.h>
 #include <time.h>
 
+const char *units[] = {"B", "kB", "MB", "GB", "TB"};
+
 int main(){
-	struct stat buffer;
-	stat("text.txt",&buffer);
-	long bytes = buffer.st_size;
-	int gigabytes = bytes / 1000000000;
-	int megabytes = (bytes - (gigabytes * 1000000000)) / 1000000;
-	bytes = bytes - (megabytes*1000000 + gigabytes*1000000000);
-	printf("size: gb:%d mb:%d b:%ld\n",gigabytes,megabytes,bytes);
-	printf("mode(permissions): %3o\n",buffer.st_mode&0777);
-	printf("time of last access: %s\n",ctime(&buffer.st_atime));
-	return 0;
+  struct stat buffer;
+  stat("text.txt",&buffer);
+  double bytes = buffer.st_size;
+
+  int i = 0;
+  while (bytes > 1000) {
+    bytes /= 1000;
+    i++;
+  }
+  printf("filesize: %.*f %s\n", i, bytes, units[i]);
+  printf("mode(permissions): %3o\n",buffer.st_mode&0777);
+  printf("time of last access: %s\n",ctime(&buffer.st_atime));
+  return 0;
 }
+
